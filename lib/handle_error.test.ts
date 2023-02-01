760f39cf59845b3c9a2849b64933ce6da09c8ead
bkiac <bkiac@pm.me>
2023-02-01 12:56:16 +0100

    Simplify error handling
    Override is removed, catchPanic renamed to recover

--- lib/handle_error.test.ts
+++ lib/handle_error.test.ts
@@ -1,32 +1,20 @@
 import {describe, expect, it} from "vitest"
-import {err} from "./core"
 import {handleError} from "./handle_error"
 import {Panic} from "./panic"
 
-class OverrideError extends Error {}
-
 describe("handleError", () => {
-	it("should use override", () => {
-		const msg = "override error message"
-		const [error] = handleError(new Error("msg"), {
-			override: () => err(new OverrideError(msg)),
-		})
-		expect(error).toBeInstanceOf(OverrideError)
-		expect(error.message).toBe(msg)
-	})
+	const msg = "message"
 
 	describe("preprocess", () => {
 		it("should preprocess unknown into error", () => {
-			const msg = "message"
 			const [error] = handleError(msg, {
 				preprocess: (e) => new Error(e as string),
 			})
 			expect(error).toBeInstanceOf(Error)
-			expect(error.message).toBe(msg)
+			expect(error.message).toEqual(msg)
 		})
 
 		it("should handle panic", () => {
-			const msg = "invalid type"
 			expect(() =>
 				handleError("not an error", {
 					preprocess: () => new Panic(msg),
@@ -35,24 +23,33 @@ describe("handleError", () => {
 		})
 	})
 
-	describe("panic", () => {
+	describe("recover", () => {
 		it("should throw on panic", () => {
-			const msg = "panic"
 			expect(() => handleError(new Panic(msg))).toThrowError(msg)
 		})
 
-		it("should not throw on panic with catchPanic true", () => {
-			const msg = "panic"
-			const [error] = handleError(new Panic(msg), {catchPanic: true})
+		it("should not throw on panic with recover true", () => {
+			const [error] = handleError(new Panic(msg), {recover: true})
 			expect(error).toBeInstanceOf(Panic)
 			expect(error.message).toBe(msg)
 		})
 	})
 
+	describe("panicOn", () => {
+		it("should throw if panicOn matches error", () => {
+			expect(() => handleError(new TypeError(msg), {panicOn: [TypeError]})).toThrowError(msg)
+		})
+
+		it("should not throw on error with panicOn not matching", () => {
+			const [error] = handleError(new Error(msg), {panicOn: [TypeError]})
+			expect(error).toBeInstanceOf(Error)
+			expect(error.message).toEqual(msg)
+		})
+	})
+
 	it("should panic on invalid type without preprocess", () => {
-		const primitive = "invalid type" // Primitive
-		expect(() => handleError(primitive)).toThrowError(primitive)
-		const object = {} // Object
-		expect(() => handleError(object)).toThrowError(object.toString())
+		;[0, "", true, null, undefined, Symbol("symbol"), {}, []].forEach((invalid) => {
+			expect(() => handleError(invalid)).toThrowError(typeof invalid)
+		})
 	})
 })

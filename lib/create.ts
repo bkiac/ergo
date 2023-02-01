0fe84370413bb0a2ec5fdabf101c631915d499c1
bkiac <bkiac@pm.me>
2023-02-01 13:57:53 +0100

    Add wrap to create

--- lib/create.ts
+++ lib/create.ts
@@ -1,12 +1,17 @@
 import {exec, execAsync} from "./exec"
 import type {ErrorHandlerOptions} from "./handle_error"
+import {wrap, wrapAsync} from "./wrap"
 
 export function create(defaultOptions?: ErrorHandlerOptions): {
 	exec: typeof exec
 	execAsync: typeof execAsync
+	wrap: typeof wrap
+	wrapAsync: typeof wrapAsync
 } {
 	return {
 		exec: (fn, options) => exec(fn, {...defaultOptions, ...options}),
 		execAsync: (fn, options) => execAsync(fn, {...defaultOptions, ...options}),
+		wrap: (fn, options) => wrap(fn, {...defaultOptions, ...options}),
+		wrapAsync: (fn, options) => wrapAsync(fn, {...defaultOptions, ...options}),
 	}
 }
